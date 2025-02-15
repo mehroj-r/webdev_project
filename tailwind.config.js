3929d5684554e56c1a69266c9796fbe7ae15a24b
Abdurauf Qarshiboyev <abduraufqarshiboyev23092005@gmail.com>
2025-02-15 06:16:28 +0500

    tailwind css is fully reinstalled to new version, postcss installed, langing home page is fully finished with its corresponding styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,26 +6,7 @@ module.exports = {
     "./src/views/main/style/main.css", // Ensure Tailwind processes this file
   ],
   theme: {
-    extend: {
-      colors: {
-        // search icon colors
-        lightpurple: "#A585FA",
-        mainpurple: "#4600FF",
-      },
-      blur: {
-        400: "400px",
-      },
-      animation: {
-        bell: "bell 0.3s ease-in-out", 
-      },
-      keyframes: {
-        bell: {
-          "0%, 100%": { transform: "rotate(0deg)" },
-          "25%": { transform: "rotate(10deg)" },
-          "75%": { transform: "rotate(-10deg)" },
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
